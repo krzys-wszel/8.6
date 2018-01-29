cd87d7cccedf29d0444f0a7cbe4bd0188e0abe85
Krzysztof Wszelaki <krzys_wszel@interia.eu>
2018-01-29 20:11:02 +0100

    Commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var a = 2;
+var b = 0;
+var value = (a * a) - (2 * a * b) + (b * b);
+if (value > 0 ){
+console.log('wynik dodatni');}
+else if (value == 0){
+console.log('wynik 0');}
+else {
+  console.log('wynik ujemny');}
