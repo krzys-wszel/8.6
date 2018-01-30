e66a2ecccd9b7d7cc9063ca1d014c56b4c992458
Krzysztof Wszelaki <krzys_wszel@interia.eu>
2018-01-30 16:25:30 +0100

    correction

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-var a = 2;
+var a = 0;
 var b = 0;
 var value = (a * a) - (2 * a * b) + (b * b);
-if (value > 0 ){
-console.log('wynik dodatni');}
-else if (value == 0){
-console.log('wynik 0');}
+if (value > 0) {
+    console.log('wynik dodatni');
+} 
+else if (value == 0) {
+    console.log('wynik 0');
+} 
+else if (value < 0) {
+    console.log('wynik ujemny');
+} 
 else {
-  console.log('wynik ujemny');}
+    console.log('wartość nieprawidłowa');
+}
